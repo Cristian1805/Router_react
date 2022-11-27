ed2a6b96e4e0cef4efcd71e5845522772350daf7
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-11-27 17:07:06 -0500

    corregido archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Contador from "./components/Contador.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-) 
+function App() {
+
+  return (
+    <div>
+      <Contador />
+    </div>
+  )
+}
+
+export default App
